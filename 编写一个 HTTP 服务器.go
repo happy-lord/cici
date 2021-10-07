2039771ddbc362212d3fac6b3047ca47dd9d300f
happy-lord <56115467+happy-lord@users.noreply.github.com>
2021-10-07 18:56:53 +0800

    Create 编写一个 HTTP 服务器.go
    编写一个 HTTP 服务器，此练习为正式作业需要提交并批改

---
+++ 编写一个 HTTP 服务器.go
@@ -0,0 +1,18 @@
+package main
+
+import (
+	"net/http"
+	"fmt"
+)
+
+func main() {
+	http.HandleFunc("/",myResponse)
+	http.ListenAndServe("127.0.0.1:8888",nil)
+}
+
+func myResponse(w http.ResponseWriter,r* http.Request)  {
+	w.Write([]byte("<html><center> <font size=\"40\">Hello！ I am go service started by myself!</font></center></html>"))
+	fmt.Println("A client has just visited!")
+}
+
+
